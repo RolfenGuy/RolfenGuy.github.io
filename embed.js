de3bd5e9fca029601877d94b38f6f8da140e5827
RolfenGuy <64381350+RolfenGuy@users.noreply.github.com>
2020-08-28 16:26:55 -0400

    Delete embed.js
    nrgbruh

--- embed.js
+++
@@ -1,1 +0,0 @@
-<div class="tenor-gif-embed" data-postid="12555861" data-share-method="host" data-width="100%" data-aspect-ratio="3.75"><a href="https://tenor.com/view/download-fleno-blink-gif-12555861">Download Fleno GIF</a> from <a href="https://tenor.com/search/download-gifs">Download GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>

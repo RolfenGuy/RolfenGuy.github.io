7ee397d79ca008aaf186655d82388ed060f4bef1
RolfenGuy <64381350+RolfenGuy@users.noreply.github.com>
2020-12-20 13:54:59 -0500

    added a little fancy dancy javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+// this is script file lol
+console.log("chances are if you're here, you are a developer. but why? why are you looking at the console? has my wesbite messed itself up? if so please contact me on discord (Rolfie #3161)");
+
+document.getElementById("btnfunky").onclick = function() {
+ console.log("funky button was just pressed")
+}
+
+document.getElementById("btnfunky").onclick = function() {
+ document.getElementById("btnfunky").style.fontSize = "40px";
+}
+
+document.getElementById("btnuseless").onclick = function() {
+ document.getElementById("heading").remove();
+}
